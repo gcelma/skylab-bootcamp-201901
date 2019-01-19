0617a3df27449565c1adf043d9f9df925c45d059
Waldo Geraldo Faldo <gcelma@MacBook-Air-de-Waldo.local>
2019-01-19 16:44:50 +0100

    added new methods

--- staff/gerard-celma/horroy/horroy.js
+++ staff/gerard-celma/horroy/horroy.js
@@ -19,5 +19,35 @@ function Horroy() {
 };
 
 Horroy.prototype.push = function(value) {
+    if (arguments.length > 1) {
+        for(var i = 0; i < arguments.length; i++) {
+            this[this.length++] = arguments[i];
+        }
+    } else {
+        this[this.length++] = value;
+    }
     
+    return this.length;
+};
+
+Horroy.prototype.pop = function() {
+    if(!this.length) {
+        return undefined;
+    }
+    var poppedValue = this[this.length - 1]
+    delete this[this.length-1];
+    this.length--;
+
+    return poppedValue;
+};
+
+Horroy.prototype.shift = function() {
+    var result = this[0];
+    for(var i = 0; i<this.length; i++) {
+        this[i] = this[i+1];
+    };
+    delete this[this.length-1];
+    this.length--;
+
+    return result;
 };
--- staff/gerard-celma/horroy/horroy.specs.js
+++ staff/gerard-celma/horroy/horroy.specs.js
@@ -1,7 +1,7 @@
 describe('Horroy', function() {
 
-    describe('arguments passed to Horroy constructor function', function() {
-        describe('--> SUCCES', function() {
+    describe('--> arguments passed to Horroy constructor function', function() {
+        describe('SUCCES', function() {
             it('should create empty Horroy when no arguments', function() {
                 var horr = new Horroy;
     
@@ -26,7 +26,7 @@ describe('Horroy', function() {
                 expect(horr.toString()).toBe({0: "KABOOM", length: 5}.toString());
             });
         });
-        describe('--> FAIL', function() {
+        describe('FAIL', function() {
             it("shouldn't create Horroy when float passed as argument", function() {
                 var error;
 
@@ -37,6 +37,59 @@ describe('Horroy', function() {
                 }
                
                 expect(error.toString()).toBe('Error: Invalid Horroy length');
+            });
+        });
+    });
+
+    describe('--> push()', function() {
+        describe('SUCCES', function() {
+            it('should create new position and add new value in horroy', function() {
+                var horr = new Horroy(1,2,3);
+                var result = horr.push(4);
+
+                expect(result).toBe(4);
+            });
+
+            it('should create 2 new positions and add 2 new values in horroy', function() {
+                var horr = new Horroy(1,2,3);
+                var result = horr.push(4,5);
+                
+                console.log(horr);
+                expect(result).toBe(5);
+                expect(horr.toString()).toBe({ 0: 1, 1: 2, 2: 3, 3: 4, 4: 5, length: 5 }.toString());
+            });
+
+        });
+        describe('FAIL', function() {
+            
+        });
+    });
+
+    describe('--> pop()', function() {
+        describe('SUCCES', function() {
+            it('should remove the last element in an array and return it.', function() {
+                var horr1 = new Horroy(1,2,3);
+                var result1 = horr1.pop();
+
+                expect(result1).toBe(3);
+            });
+            it('should return undefined if horoy is empty', function() {
+                var horr1 = new Horroy();
+                var result1 = horr1.pop();
+
+                expect(result1).toBe(undefined);
+            });
+        });
+    });
+
+    describe('--> shift()', function() {
+        describe('SUCCES', function() {
+            it('should delete and return the first element of an horroy.', function() {
+                var horr1 = new Horroy(1,2,3);
+                var result1 = horr1.shift();
+
+                expect(result1).toBe(1);
+                expect(horr1.toString()).toBe({ 0: 2, 1: 3, length: 2 }.toString());
             });
         });
     });
