84134edecb956f24d8b287a982ef45c7cde2d119
Waldo Geraldo Faldo <gcelma@MacBook-Air-de-Waldo.local>
2019-02-15 14:47:59 +0100

    stable version from MBarzi

--- staff/gerard-celma/web-app/src/logic/index.spec.js
+++ staff/gerard-celma/web-app/src/logic/index.spec.js
@@ -1,15 +1,15 @@
 'use strict'
-
 const { expect } = require('chai')
-const logic = require('.')
+const Logic = require('.')
 
-describe('logic', () => {
+describe('Logic', () => {
     describe('register user', () => {
-        const name = 'Manolo'
-        const surname = 'Skywalker'
-        const email = `register@mail.com-${Math.random()}`
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const email = `manuelbarzi@mail.com-${Math.random()}`
         const password = '123'
         const passwordConfirm = password
+        const logic = new Logic
 
         it('should succeed on valid data', () =>
             logic.registerUser(name, surname, email, password, passwordConfirm)
@@ -18,10 +18,10 @@ describe('logic', () => {
 
         it('should fail on undefined name', () => {
             const name = undefined
-            const surname = 'Skywalker'
-            const email = 'manoloskywalker@mail.com'
+            const surname = 'Barzi'
+            const email = 'manuelbarzi@mail.com'
             const password = '123'
-            debugger
+
             expect(() => {
                 logic.registerUser(name, surname, email, password, password)
             }).to.throw(TypeError, name + ' is not a string')
@@ -29,8 +29,8 @@ describe('logic', () => {
 
         it('should fail on numeric name', () => {
             const name = 10
-            const surname = 'Skywalker'
-            const email = 'manoloskywalker@mail.com'
+            const surname = 'Barzi'
+            const email = 'manuelbarzi@mail.com'
             const password = '123'
 
             expect(() => {
@@ -41,8 +41,8 @@ describe('logic', () => {
 
         it('should fail on boolean name', () => {
             const name = true
-            const surname = 'Skywalker'
-            const email = 'manoloskywalker@mail.com'
+            const surname = 'Barzi'
+            const email = 'manuelbarzi@mail.com'
             const password = '123'
 
             expect(() => {
@@ -52,8 +52,8 @@ describe('logic', () => {
 
         it('should fail on object name', () => {
             const name = {}
-            const surname = 'Skywalker'
-            const email = 'manoloskywalker@mail.com'
+            const surname = 'Barzi'
+            const email = 'manuelbarzi@mail.com'
             const password = '123'
 
             expect(() => {
@@ -63,8 +63,8 @@ describe('logic', () => {
 
         it('should fail on array name', () => {
             const name = []
-            const surname = 'Skywalker'
-            const email = 'manoloskywalker@mail.com'
+            const surname = 'Barzi'
+            const email = 'manuelbarzi@mail.com'
             const password = '123'
 
             expect(() => {
@@ -74,19 +74,19 @@ describe('logic', () => {
 
         it('should fail on empty name', () => {
             const name = ''
-            const surname = 'Skywalker'
-            const email = 'manoloskywalker@mail.com'
+            const surname = 'Barzi'
+            const email = 'manuelbarzi@mail.com'
             const password = '123'
 
             expect(() => {
                 logic.registerUser(name, surname, email, password, password)
-            }).to.throw(Error,'name cannot be empty')
+            }).to.throw(Error, 'name cannot be empty')
         })
 
         it('should fail on undefined surname', () => {
-            const name = 'Manolo'
+            const name = 'Manuel'
             const surname = undefined
-            const email = 'manoloskywalker@mail.com'
+            const email = 'manuelbarzi@mail.com'
             const password = '123'
 
             expect(() => {
@@ -95,9 +95,9 @@ describe('logic', () => {
         })
 
         it('should fail on numeric surname', () => {
-            const name = 'Manolo'
+            const name = 'Manuel'
             const surname = 10
-            const email = 'manoloskywalker@mail.com'
+            const email = 'manuelbarzi@mail.com'
             const password = '123'
 
             expect(() => {
@@ -107,9 +107,9 @@ describe('logic', () => {
 
 
         it('should fail on boolean surname', () => {
-            const name = 'Manolo'
+            const name = 'Manuel'
             const surname = false
-            const email = 'manoloskywalker@mail.com'
+            const email = 'manuelbarzi@mail.com'
             const password = '123'
 
             expect(() => {
@@ -118,9 +118,9 @@ describe('logic', () => {
         })
 
         it('should fail on object surname', () => {
-            const name = 'Manolo'
+            const name = 'Manuel'
             const surname = {}
-            const email = 'manoloskywalker@mail.com'
+            const email = 'manuelbarzi@mail.com'
             const password = '123'
 
             expect(() => {
@@ -129,9 +129,9 @@ describe('logic', () => {
         })
 
         it('should fail on array surname', () => {
-            const name = 'Manolo'
+            const name = 'Manuel'
             const surname = []
-            const email = 'manoloskywalker@mail.com'
+            const email = 'manuelbarzi@mail.com'
             const password = '123'
 
             expect(() => {
@@ -140,198 +140,100 @@ describe('logic', () => {
         })
 
         it('should fail on empty surname', () => {
-            const name = 'Manolo'
+            const name = 'Manuel'
             const surname = ''
-            const email = 'manoloskywalker@mail.com'
+            const email = 'manuelbarzi@mail.com'
             const password = '123'
 
             expect(() => {
                 logic.registerUser(name, surname, email, password, password)
-            }).to.throw(Error,'surname cannot be empty')
+            }).to.throw(Error, 'surname cannot be empty')
         })
+    })
 
+    describe('log in user', () => {
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const email = `manuelbarzi@mail.com-${Math.random()}`
+        const password = '123'
+        const passwordConfirm = password
+        const logic = new Logic
 
+        beforeEach(() =>
+            logic.registerUser(name, surname, email, password, passwordConfirm)
+        )
+
+        it('should succeed on correct credentials', () =>
+            logic.logInUser(email, password)
+                .then(() => {
+                    expect(logic.__storage__.userId).to.exist
+                    expect(logic.__storage__.userApiToken).to.exist
+                })
+        )
+    })
+
+    describe('check user is logged in', () => {
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const email = `manuelbarzi@mail.com-${Math.random()}`
+        const password = '123'
+        const passwordConfirm = password
+        const logic = new Logic
+
+        beforeEach(() =>
+            logic.registerUser(name, surname, email, password, passwordConfirm)
+        )
+
+        it('should succeed on correct credentials', () =>
+            logic.logInUser(email, password)
+                .then(() => expect(logic.isUserLoggedIn).to.be.true)
+        )
+    })
+
+    describe('log out user', () => {
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const email = `manuelbarzi@mail.com-${Math.random()}`
+        const password = '123'
+        const passwordConfirm = password
+        const logic = new Logic
+
+        beforeEach(() =>
+            logic.registerUser(name, surname, email, password, passwordConfirm)
+                .then(() => logic.logInUser(email, password))
+        )
+
+        it('should succeed on correct credentials', () => {
+            logic.logOutUser()
+
+            expect(logic.__storage__.userId).to.be.null
+            expect(logic.__storage__.userId).to.be.null
+        })
     })
 
     describe('retrieve user', () => {
-        const name = 'Manolo'
-        const surname = 'Skywalker'
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const email = `manuelbarzi@mail.com-${Math.random()}`
         const password = '123'
         const passwordConfirm = password
-        let email
+        const logic = new Logic
 
-        beforeEach(() => {
-            email = `retrieve@mail.com-${Math.random()}`
-            return logic.registerUser(name, surname, email, password, passwordConfirm)
+        beforeEach(() =>
+            logic.registerUser(name, surname, email, password, passwordConfirm)
                 .then(() => logic.logInUser(email, password))
-        })
+        )
 
         it('should succeed on correct credentials', () =>
             logic.retrieveUser()
                 .then(user => {
-                    expect(user.id).to.equal(logic.__userId__)
+                    expect(user.id).to.equal(logic.__storage__.userId)
                     expect(user.name).to.equal(name)
                     expect(user.surname).to.equal(surname)
                     expect(user.email).to.equal(email)
                 })
         )
-
-        it('should fail on undefined', () => {
-
-            try {
-                logic.retrieveUser()
-
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal(`undefined is not a string`)
-            }
-        })
-
-        it('should fail on empty string', () => {
-
-            try {
-                logic.retrieveUser('')
-
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal(`undefined is not a string`)
-            }
-        })
     })
 
-
-
-    describe('login user', () => {
-
-        const name = 'Manolo'
-        const surname = 'Skywalker'
-        const password = '123'
-        const passwordConfirm = password
-        let email
-
-        beforeEach(() => {
-            email = `retrieve@mail.com-${Math.random()}`
-            return logic.registerUser(name, surname, email, password, passwordConfirm)
-        })
-
-        it('login should succeed on correct credentials', () => {
-
-            return logic.logInUser(email, password)
-                .then(() => {
-                    expect(logic.__userId__).to.exist
-                    expect(logic.__userApiToken__).to.exist
-                })
-        })
-
-        it('should fail on a wrong email', () => {
-
-            const em = `login@fake.com`
-            const pass = '123'
-
-            return logic.logInUser(em, pass).catch(error => {
-                expect(error).to.exist
-            })
-
-
-        })
-
-        it('should fail on undefined', () => {
-
-            try {
-                logic.logInUser()
-
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal(`undefined is not a string`)
-            }
-        })
-
-
-        it('should fail if email is empty', () => {
-
-            const email = ''
-            const password = '123'
-
-            try {
-                logic.logInUser(email, password)
-
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('email cannot be empty')
-            }
-        })
-
-        it('should fail if password is empty', () => {
-
-            const email = `login@fake.com`
-            const password = ''
-
-            try {
-                logic.logInUser(email, password)
-
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('password cannot be empty')
-            }
-        })
-    })
-
-
-
-
-    describe('updateUser', () => {
-
-        let name = 'Manolo'
-        let surname = 'Skywalker'
-        let email
-        let password = '123'
-        let passwordConfirm = '123'
-        let favouriteId
-        let data = { data: 'data' }
-
-
-        beforeEach(() => {
-
-            email = `check@favourite.com-${Math.random()} `
-            favouriteId = `favIdToggle - ${Math.random()} `
-
-            return logic.registerUser(name, surname, email, password, passwordConfirm)
-                .then(() => logic.logInUser(email, password))
-
-        })
-
-
-        it(`should be undefined with { data: 'data' } as a variable`, () => {
-            try {
-                logic.updateUser(data)
-            } catch (error) {
-                expect(error).to.exist
-            }
-        }
-        )
-
-
-        it('should fail on empty string', () => {
-
-            try {
-                logic.updateUser('')
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('logic.updateUser is not a function')
-
-            }
-        })
-
-        it('should fail on undefined', () => {
-
-            try {
-                logic.updateUser()
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal(`logic.updateUser is not a function`)
-
-            }
-        })
-    })
-
+    // TODO updateUser and removeUser
 })
--- staff/gerard-celma/web-app/src/user-api/index.spec.js
+++ staff/gerard-celma/web-app/src/user-api/index.spec.js
@@ -4,26 +4,16 @@ const { expect } = require('chai')
 const userApi = require('.')
 
 describe('user api', () => {
-
     describe('register', () => {
-        const name = 'Manolo'
-        const surname = 'Skywalker'
-        const username = `ManoloSkywalker-${Math.random()}`
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const username = `manuelbarzi-${Math.random()}`
         const password = '123'
 
         it('should succeed on correct data', () =>
             userApi.register(name, surname, username, password)
                 .then(id => expect(id).to.exist)
         )
-
-        it('should fail on undefined', () => {
-            try {
-                userApi.register()
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal(`undefined is not a string`)
-            }
-        })
 
         it('should fail on already existing user', () =>
             userApi.register(name, surname, username, password)
@@ -36,72 +26,20 @@ describe('user api', () => {
                 })
         )
 
-        it('should fail on empty name', () => {
-
-            const name = ''
-
-            try {
-                userApi.register(name, surname, username, password)
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('name is empty')
-            }
-        })
-
-        it('should fail on empty username', () => {
-
-            const username = ''
-
-            try {
-                userApi.register(name, surname, username, password)
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('username is empty')
-            }
-        })
-
-
-        it('should fail on empty surname', () => {
-
-            const surname = ''
-
-            try {
-                userApi.register(name, surname, username, password)
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('surname is empty')
-            }
-        })
-
-
-        it('should fail on empty password', () => {
-
-            const password = ''
-
-            try {
-                userApi.register(name, surname, username, password)
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('password is empty')
-            }
-        })
-
         // TODO more unit test cases
     })
 
     describe('authenticate', () => {
-        let name = 'Manolo'
-        let surname = 'Skywalker'
-        let username
-        let password
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const username = `manuelbarzi-${Math.random()}`
+        const password = '123'
+
         let _id
 
-        beforeEach(() => {
-            username = `ManoloSkywalker-${Math.random()}`
-            password = '123'
-            return userApi.register(name, surname, username, password)
+        beforeEach(() =>
+            userApi.register(name, surname, username, password)
                 .then(id => _id = id)
-        }
         )
 
         it('should succeed on correct data', () =>
@@ -112,60 +50,22 @@ describe('user api', () => {
                 })
         )
 
-        it('should fail on undefined', () => {
-
-            try {
-                userApi.authenticate()
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal(`undefined is not a string`)
-            }
-        })
-
-
-        it('should fail on empty username', () => {
-
-
-            try {
-                userApi.authenticate('', password)
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('username is empty')
-            }
-        })
-
-
-        it('should fail on empty password', () => {
-
-
-            try {
-                userApi.authenticate(username, '')
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('password is empty')
-            }
-        })
-
         // TODO more unit test cases
     })
 
     describe('retrieve', () => {
-        let name = 'Manolo'
-        let surname = 'Skywalker'
-        let username
-        let password = '123'
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const username = `manuelbarzi-${Math.random()}`
+        const password = '123'
 
         let _id, _token
 
-        beforeEach(() => {
-
-            username = `ManoloSkywalker-${Math.random()}`
-
-            return userApi.register(name, surname, username, password)
+        beforeEach(() =>
+            userApi.register(name, surname, username, password)
                 .then(id => _id = id)
                 .then(() => userApi.authenticate(username, password))
                 .then(({ token }) => _token = token)
-        }
         )
 
         it('should succeed on correct data', () =>
@@ -178,67 +78,26 @@ describe('user api', () => {
                 })
         )
 
-        it('should fail on undefined', () => {
-
-            try {
-                userApi.retrieve()
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal(`undefined is not a string`)
-            }
-        })
-
-        it('should fail on empty id', () => {
-
-            let _id = ''
-            let _token = 'token'
-
-            try {
-                userApi.retrieve(_id, _token)
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('id is empty')
-            }
-        })
-
-        it('should fail on empty token', () => {
-
-            let _id = 'id'
-            let _token = ''
-
-            try {
-                userApi.retrieve(_id, _token)
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('token is empty')
-            }
-        })
-
-
-
         // TODO more unit test cases
     })
 
     describe('update', () => {
-        let name = 'Manolo'
-        let surname = 'Skywalker'
-        let username
-        let password = '123'
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const username = `manuelbarzi-${Math.random()}`
+        const password = '123'
 
         let _id, _token
 
-        beforeEach(() => {
-
-            username = `ManoloSkywalker-${Math.random()}`
-
-            return userApi.register(name, surname, username, password)
+        beforeEach(() =>
+            userApi.register(name, surname, username, password)
                 .then(id => _id = id)
                 .then(() => userApi.authenticate(username, password))
                 .then(({ token }) => _token = token)
-        }
         )
+
         it('should succeed on correct data', () => {
-            let data = { name: 'Pepito', surname: 'Grillo', age: 32 }
+            const data = { name: 'Pepito', surname: 'Grillo', age: 32 }
 
             return userApi.update(_id, _token, data)
                 .then(() => userApi.retrieve(_id, _token))
@@ -251,151 +110,33 @@ describe('user api', () => {
                 })
         })
 
-        it('should fail on undefined', () => {
-
-            try {
-                userApi.update()
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal(`undefined is not a string`)
-            }
-        })
-
-        it('should fail on empty id', () => {
-
-            try {
-                userApi.update('')
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('id is empty')
-            }
-        })
-
-        it('should fail on empty token', () => {
-
-            let id = 'id'
-            let token = ''
-            let data = 'data'
-
-            try {
-                userApi.update(id, token, data)
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('token is empty')
-            }
-        })
-
-
-
-
-
-
-
         // TODO more unit test cases
     })
 
     describe('remove', () => {
-        let name = 'Manolo'
-        let surname = 'Skywalker'
-        let username
-        let password = '123'
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const username = `manuelbarzi-${Math.random()}`
+        const password = '123'
 
         let _id, _token
 
-        beforeEach(() => {
-            username = `ManoloSkywalker-${Math.random()}`
-
-            return userApi.register(name, surname, username, password)
+        beforeEach(() =>
+            userApi.register(name, surname, username, password)
                 .then(id => _id = id)
                 .then(() => userApi.authenticate(username, password))
                 .then(({ token }) => _token = token)
-        }
         )
 
-
-
         it('should succeed on correct data', () => {
-
             return userApi.remove(_id, _token, username, password)
                 .then(() => userApi.retrieve(_id, _token))
                 .then(() => {
                     throw Error('should not pass by here')
                 })
-                .catch(({ message }) => expect(message).to.equal(`user with id \"${_id}\" does not exist`))
+                .catch(({message}) => expect(message).to.equal(`user with id \"${_id}\" does not exist`))
         })
 
-
-        it('should fail on undefined', () => {
-
-            try {
-                userApi.remove(_id, _token, username, password)
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal(`undefined is not a string`)
-            }
-        })
-
-        it('should fail on empty id', () => {
-
-            let _id = ''
-            let _token = 'token'
-            let username = 'username'
-            let password = '123'
-
-            try {
-                userApi.remove(_id, _token, username, password)
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('id is empty')
-            }
-        })
-
-
-        it('should fail on empty token', () => {
-
-            let _id = 'id'
-            let _token = ''
-            let username = 'username'
-            let password = '123'
-
-            try {
-                userApi.remove(_id, _token, username, password)
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('token is empty')
-            }
-        })
-
-        it('should fail on empty username', () => {
-
-            let _id = 'id'
-            let _token = 'token'
-            let username = ''
-            let password = '123'
-
-            try {
-                userApi.remove(_id, _token, username, password)
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('username is empty')
-            }
-        })
-
-
-        it('should fail on empty password', () => {
-
-            let _id = 'id'
-            let _token = 'token'
-            let username = 'username'
-            let password = ''
-
-            try {
-                userApi.remove(_id, _token, username, password)
-            } catch (error) {
-                expect(error).to.exist
-                expect(error.message).to.equal('password is empty')
-            }
-        })
         // TODO more unit test cases
     })
 })
